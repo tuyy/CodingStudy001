b406bf13be9c5887fcae51edd7062f5f14f8edf0
taeun.ju <taeun.ju@worksmobile.com>
2019-07-22 09:15:54 +0900

    First

---
+++ README.md
@@ -0,0 +1,3 @@
+### 프로그래밍 패턴 - 프로그램을 작성하는 33가지 방법
+* 지은이 : 크리스티나 로페즈
+* https://github.com/crista/exercises-in-programming-style
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "001/C001.h"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    C001::process();
+
+    return 0;
+}
